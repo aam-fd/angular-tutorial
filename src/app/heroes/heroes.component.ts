8048f1c1ac029adaeacb1dfd9ccbba2b0b762c36
Andreeva Anastasia <32935561+aam-fd@users.noreply.github.com>
2019-05-18 22:43:07 +0300

    2. display a Heroes List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero.class';
+import {HEROES} from '../heroes.mock';
 
 // @Component is a decorator function that specifies the Angular metadata for the component.
 @Component({
@@ -14,11 +15,19 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
   constructor() { }
 
   // lifecycle hook.
   // Angular calls ngOnInit shortly after creating a component. It's a good place to put initialization logic.
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
